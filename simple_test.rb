5adf4e95f31e72fc05af0eded304abd1a8204f8c
chrislample <clample@testobject.com>
2016-01-27 11:21:17 +0100

    Read api_key from environment variable. Add basic login test.

--- simple_test.rb
+++ simple_test.rb
@@ -1,14 +1,14 @@
-require 'rubygems'
 require 'appium_lib'
 
 desired_caps = {
   caps:       {
-    testobject_api_key: 'YOUR_TESTOBJECT_API_KEY',
+    testobject_api_key: ENV['TESTOBJECT_API_KEY'],
     testobject_app_id: '1',
     testobject_device: 'Motorola_Moto_G_2nd_gen_real'
   },
   appium_lib: {
-    server_url: 'https://app.testobject.com:443/api/appium/wd/hub'
+    server_url: 'https://app.testobject.com:443/api/appium/wd/hub',
+    wait: 10
   }
 }
 
@@ -22,6 +22,12 @@ module Komoot
     Appium.promote_singleton_appium_methods Komoot
 
     find_element(:id, 'de.komoot.android:id/button_mail_login').click
+    find_element(:id, 'de.komoot.android:id/txt_user_name').send_keys("jfjfkdl")
+    find_element(:id, 'de.komoot.android:id/txt_password').send_keys("lakjsdfja")
+    find_element(:id, 'de.komoot.android:id/btn_login').click
+
+    find_element(:id, "android:id/message")
+
 
     # Quit when you're done!
     driver_quit
